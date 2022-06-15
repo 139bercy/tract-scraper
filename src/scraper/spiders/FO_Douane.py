a7313a2ad8f5e86cf7a8b81a6666ebcbcd6477e9
kaa-serpent <guillaumedecadoudal@gmail.com>
2022-06-15 16:53:12 +0200

    update: fo douanes

--- src/scraper/spiders/FO_Douane.py
+++ src/scraper/spiders/FO_Douane.py
@@ -4,17 +4,17 @@ from scraper.spiders.BaseSpider import BaseSpider
 class FO_Douane(BaseSpider):
     name = "FO_Douane"
     
-    article_selector_in_list = "table table"
-    article_link_selector_in_list = None
-    article_title_selector_in_list = "strong::text"
-    article_date_selector_in_list = "font::text"
+    article_selector_in_list = ".lcp_catlist"
+    article_link_selector_in_list = "li a::attr(href)"
+    article_title_selector_in_list = "li a::text"
+    article_date_selector_in_list = "li::text"
     article_date_format_in_list = "%d %B %Y"
-    article_html_selector = "table table"
-    article_file_selector = "a::attr(href)"
+    article_html_selector = "div.entry-content p"
+    article_file_selector = None
 
     page_index = 1
     page_selector = None
 
     start_urls = [
-        'http://fodouanes.fr/actualites.html'
+        'https://fodouanes.fr/sndfo/les-actualites-du-snd-fo/'
     ]
