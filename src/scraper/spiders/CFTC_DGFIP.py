347461deb09fbf2c8adc209af261d9f5fd6e3a2f
kaa-serpent <guillaumedecadoudal@gmail.com>
2022-09-14 17:52:55 +0200

    feat : date adaptation

--- src/scraper/spiders/CFTC_DGFIP.py
+++ src/scraper/spiders/CFTC_DGFIP.py
@@ -8,9 +8,24 @@ class CFTC_DGFIP(BaseSpider):
     article_link_selector_in_list = ".entry-header .entry-title a ::attr(href)"
     article_title_selector_in_list = ".entry-header .entry-title a ::text"
     article_date_selector_in_list = ".entry-footer .entry-date ::text"
-    article_date_format_in_list = "%d %B %Y"
+    article_date_format_in_list = "%d %m %Y"
     article_html_selector = "article"
     article_file_selector = '.wp-block-file a ::attr(href)'
+
+    article_date_term_in_list = {
+        "janvier": "01",
+        "février": "02",
+        "mars": "03",
+        "avril": "04",
+        "mai": "05",
+        "juin": "06",
+        "juillet": "07",
+        "août": "08",
+        "septembre": "09",
+        "octobre": "10",
+        "novembre": "11",
+        "décembre": "12",
+    }
 
     page_index = 1
     page_selector = 'page/{page}'
