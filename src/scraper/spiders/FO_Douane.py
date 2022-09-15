7c34b82083d9c2958ccbbec805ac7824af50c3af
kaa-serpent <guillaumedecadoudal@gmail.com>
2022-09-15 12:11:32 +0200

    fix : fo_douane selector date

--- src/scraper/spiders/FO_Douane.py
+++ src/scraper/spiders/FO_Douane.py
@@ -7,10 +7,12 @@ class FO_Douane(BaseSpider):
     article_selector_in_list = ".lcp_catlist"
     article_link_selector_in_list = "li a ::attr(href)"
     article_title_selector_in_list = "li a ::text"
-    article_date_selector_in_list = "li ::text"
+    article_date_selector_in_list = "li"
+    article_date_separator_in_list = "</a>"
     article_date_format_in_list = "%d %B %Y"
     article_html_selector = "div.entry-content p"
-    article_file_selector = None
+    article_date_unwanted_words = ["</li>"]
+    article_file_selector = ".wp-block-file__button"
 
     page_index = 1
     page_selector = None
